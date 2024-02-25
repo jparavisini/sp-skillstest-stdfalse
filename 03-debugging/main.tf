023397339fbc63806b4b0f78d47789874723115f
Egor Abramov <stdfalse@gmail.com>
2024-02-26 00:34:54 +0100

    Post-apply debug. Formatting and deploying in public subnets with ssh access

--- 03-debugging/main.tf
+++ 03-debugging/main.tf
@@ -1,9 +1,19 @@
+# VPC configuration
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
 
 resource "aws_subnet" "public_subnet_1" {
@@ -20,24 +30,17 @@ resource "aws_subnet" "public_subnet_2" {
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.10.0/24"
-  availability_zone       = "us-west-1a"
-  map_public_ip_on_launch = false
+resource "aws_route_table_association" "public_subnet_1_route_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.4.0/24"
-  availability_zone       = "us-west-1c"
-  map_public_ip_on_launch = false
+resource "aws_route_table_association" "public_subnet_2_route_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_eip" "nat_eip" {
-  vpc = true
-}
-
+# ALB and EC2 security groups
 resource "aws_security_group" "alb_sg" {
   name        = "alb_sg"
   description = "Allow inbound traffic"
@@ -65,11 +68,19 @@ resource "aws_security_group" "ec2_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description     = "Traffic from ALB"
+    description     = "Incoming ALB traffic"
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    description = "Incoming SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -80,12 +91,29 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
+# ALB, TG and ASG
 resource "aws_lb" "alb" {
   name               = "alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  security_groups = [
+    aws_security_group.alb_sg.id
+  ]
+  subnets = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
 }
 
 resource "aws_lb_target_group" "tg" {
@@ -93,17 +121,6 @@ resource "aws_lb_target_group" "tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
-}
-
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
 }
 
 data "aws_ami" "ubuntu" {
@@ -124,7 +141,8 @@ data "aws_ami" "ubuntu" {
 resource "aws_launch_template" "launch_template" {
   name_prefix   = "launch_template"
   image_id      = data.aws_ami.ubuntu.id
-  instance_type = "t3a.small"
+  instance_type = "t2.micro"
+  key_name      = "my-key-pair"
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
@@ -140,16 +158,19 @@ EOF
 }
 
 resource "aws_autoscaling_group" "asg" {
-  desired_capacity  = 1
-  max_size          = 5
-  min_size          = 1
+  desired_capacity = 1
+  max_size         = 5
+  min_size         = 1
   target_group_arns = [aws_lb_target_group.tg.arn]
   launch_template {
     id      = aws_launch_template.launch_template.id
     version = "$Latest"
   }
 
-  vpc_zone_identifier = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  vpc_zone_identifier = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
 }
 
 output "endpoint" {
