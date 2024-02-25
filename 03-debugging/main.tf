639b3362fcee48648506e4e6e59c62d25f2ca971
Egor Abramov <stdfalse@gmail.com>
2024-02-26 00:24:16 +0100

    Pre-apply. Change ALB ingress from HTTPS to HTTP

--- 03-debugging/main.tf
+++ 03-debugging/main.tf
@@ -44,9 +44,9 @@ resource "aws_security_group" "alb_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "HTTPS from anywhere"
-    from_port   = 443
-    to_port     = 443
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
