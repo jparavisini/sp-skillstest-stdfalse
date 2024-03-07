56a2bc1c80025c09c3504ece33a290f1bbe360d3
Egor Abramov <egor@snowplowanalytics.com>
2024-03-07 17:55:13 +0100

    Change region 2

--- 03-debugging/main.tf
+++ 03-debugging/main.tf
@@ -18,28 +18,28 @@ resource "aws_eip" "nat_eip_2" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-1c"
+  availability_zone       = "us-east-1c"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.10.0/24"
-  availability_zone       = "us-west-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.4.0/24"
-  availability_zone       = "us-west-1c"
+  availability_zone       = "us-east-1c"
   map_public_ip_on_launch = false
 }
 
